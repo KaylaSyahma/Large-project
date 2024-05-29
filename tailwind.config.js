094c0e3f0ce553a393ee64344e979c66633b9b03
KaylaSyahma <ksyahma22@gmail.com>
2024-05-29 07:42:05 +0700

    setting tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,22 @@ export default {
         },
     },
 
-    plugins: [forms],
+    screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1330px',
+    },
+
+    boxShadow: {
+        custom1: '0px 2px 40px 0px rgba(8, 70, 78, 0.08)',
+        custom2: '0px 2px 40px 0px rgba(8, 73, 81, 0.06)',
+        custom3: '0px 4.44px 4.44px 0px rgba(0, 0, 0, 0.25)',
+    },
+
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
+    
 };

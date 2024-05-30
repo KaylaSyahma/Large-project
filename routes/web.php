a5159f9318ae99164c187b6a6ccbb1ae232d146b
KaylaSyahma <ksyahma22@gmail.com>
2024-05-30 13:46:26 +0700

    bikin middleware admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function(){
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/dashboard', 'index');
+    });
 });
 
 require __DIR__.'/auth.php';
